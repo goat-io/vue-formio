486336e3879fc7af704fc8b249b244f7d46ca361
Tomas Castro <tcastrog10@gmail.com>
2019-09-04 17:15:49 -0400

    fixed types

--- src/components/Form.ts
+++ src/components/Form.ts
@@ -1,176 +1,212 @@
 /* globals console, Promise */
-import Vue from 'vue';
-import { Component, Prop, Watch } from 'vue-property-decorator';
-import AllComponents from 'formiojs/components';
-import Components from 'formiojs/components/Components';
-import { Submission, Form } from '@goatlab/goatjs';
-Components.setComponents(AllComponents);
-import FormioForm from 'formiojs/Form';
-import Formio from 'formiojs/Formio';
-const pluralize = require('pluralize');
+import Vue from 'vue'
+import { Component, Prop, Watch } from 'vue-property-decorator'
+import AllComponents from 'formiojs/components'
+import Components from 'formiojs/components/Components'
+import { Submission, Form } from '@goatlab/goatjs'
+Components.setComponents(AllComponents)
+import FormioForm from 'formiojs/Form'
+import Formio from 'formiojs/Formio'
+const pluralize = require('pluralize')
 
 @Component
 export default class extends Vue {
-  formio?: Formio;
+  formio?: Formio
 
   @Prop()
-  src?: string;
+  src?: string
 
   @Prop()
-  url?: string;
+  url?: string
 
   @Prop()
-  form?: object;
+  form?: object
 
   @Prop()
-  APP_URL?: string;
+  APP_URL?: string
 
   @Prop()
-  LOOPBACK_URL?: string;
+  LOOPBACK_URL?: string
 
   @Prop()
-  submission?: object;
+  submission?: object
 
   @Prop()
-  language?: string;
+  language?: string
 
   @Prop({ default: () => {} })
-  options?: object;
+  options?: object
+  _uid?: any
 
   @Watch('src')
   srcChange(value: string) {
     if (this.formio) {
-      this.formio.src = value;
+      this.formio.src = value
     }
   }
 
   @Watch('url')
   urlChange(value: string) {
     if (this.formio) {
-      this.formio.url = value;
+      this.formio.url = value
     }
   }
 
   @Watch('form')
   formChange(value: object) {
     if (this.formio) {
-      this.formio.form = value;
+      this.formio.form = value
     }
   }
 
   @Watch('submission')
   submissionhange(value: object) {
     if (this.formio) {
-      this.formio.submission = value;
+      this.formio.submission = value
     }
   }
 
   @Watch('language')
   languageChange(value: string) {
     if (this.formio) {
-      this.formio.language = value;
+      this.formio.language = value
     }
   }
 
   async mounted() {
-    let localForms = await Form.local().get();
+    let localForms = await Form.local().get()
     localForms = localForms.reduce((r, form) => {
-      r[form.data._id] = form.data.path;
-      return r;
-    }, {});
+      r[form.data._id] = form.data.path
+      return r
+    }, {})
     const loopbackGetPlugin = {
       priority: 0,
       preRequest: async request => {
-        if (!(request.method === 'GET') || !request.url.includes(this.APP_URL)) {
-          return undefined;
+        if (
+          !(request.method === 'GET') ||
+          !request.url.includes(this.APP_URL)
+        ) {
+          return undefined
         }
       },
       request: () => {
-        console.log('request');
+        console.log('request')
       },
       preStaticRequest: () => {
-        console.log('preStaticRequest');
+        console.log('preStaticRequest')
       },
       staticRequest: async request => {
-        if (!(request.method === 'GET') || !request.url.includes(this.APP_URL)) {
-          return undefined;
+        if (
+          !(request.method === 'GET') ||
+          !request.url.includes(this.APP_URL)
+        ) {
+          return undefined
         }
 
-        const { url } = request;
+        const { url } = request
         const formPath =
-          localForms[url.substring(url.lastIndexOf('/form/') + 6, url.lastIndexOf('/submission'))];
+          localForms[
+            url.substring(
+              url.lastIndexOf('/form/') + 6,
+              url.lastIndexOf('/submission')
+            )
+          ]
         const lbQueryUrl = {
           base: this.LOOPBACK_URL,
           path: formPath,
           formField: pluralize.singular(formPath),
-          limit: Number(url.substring(url.lastIndexOf('?limit=') + 7, url.lastIndexOf('&skip'))),
+          limit: Number(
+            url.substring(
+              url.lastIndexOf('?limit=') + 7,
+              url.lastIndexOf('&skip')
+            )
+          ),
           filter: url.substring(url.lastIndexOf('&filter=') + 8, url.length),
           where: url.includes('&where')
             ? decodeURIComponent(
-                url.substring(url.lastIndexOf('&where') + 6, url.lastIndexOf('__regex'))
+                url.substring(
+                  url.lastIndexOf('&where') + 6,
+                  url.lastIndexOf('__regex')
+                )
               ).replace('=', '')
             : undefined,
           searchString: url.includes('&where')
             ? decodeURIComponent(
-                url.substring(url.lastIndexOf('__regex') + 8, url.lastIndexOf('&filter='))
+                url.substring(
+                  url.lastIndexOf('__regex') + 8,
+                  url.lastIndexOf('&filter=')
+                )
               )
             : undefined
-        };
+        }
 
         // Make the field searchable
         if (lbQueryUrl.searchString && lbQueryUrl.where) {
-          lbQueryUrl.where = lbQueryUrl.where.replace('{{input}}', `"${lbQueryUrl.searchString}"`);
+          lbQueryUrl.where = lbQueryUrl.where.replace(
+            '{{input}}',
+            `"${lbQueryUrl.searchString}"`
+          )
         }
         try {
           if (lbQueryUrl.filter) {
-            lbQueryUrl.filter = lbQueryUrl.filter ? JSON.parse(lbQueryUrl.filter) : undefined;
+            lbQueryUrl.filter = lbQueryUrl.filter
+              ? JSON.parse(lbQueryUrl.filter)
+              : undefined
           }
         } catch (error) {
           // console.log('Could not parse FILTER one of your resource queries: ', formPath, url);
-          return undefined;
+          return undefined
         }
         try {
           if (lbQueryUrl.where) {
-            lbQueryUrl.where = lbQueryUrl.where ? JSON.parse(lbQueryUrl.where) : undefined;
-            lbQueryUrl.filter.where = lbQueryUrl.where;
+            lbQueryUrl.where = lbQueryUrl.where
+              ? JSON.parse(lbQueryUrl.where)
+              : undefined
+            lbQueryUrl.filter.where = lbQueryUrl.where
           } else {
-            const currentValue = this.formio.submission.data[lbQueryUrl.formField];
+            const currentValue = this.formio.submission.data[
+              lbQueryUrl.formField
+            ]
             if (currentValue) {
-              lbQueryUrl.where = { _id: currentValue };
-              lbQueryUrl.filter.where = lbQueryUrl.where;
+              lbQueryUrl.where = `{ _id: ${currentValue} }`
+              lbQueryUrl.filter.where = lbQueryUrl.where
             }
           }
         } catch (error) {
-          console.log('Could not parse WHERE one of your resource queries: ', formPath, url);
-          return undefined;
+          console.log(
+            'Could not parse WHERE one of your resource queries: ',
+            formPath,
+            url
+          )
+          return undefined
         }
         const result = await Submission({ path: lbQueryUrl.path })
           .remote({ connectorName: 'loopback' })
           .raw(lbQueryUrl.filter)
           .populate(lbQueryUrl.filter.related)
-          .get();
+          .get()
 
-        return result;
+        return result
       }
-    };
-    Formio.setBaseUrl(this.APP_URL);
-    Formio.registerPlugin(loopbackGetPlugin, `moveCallsToLoopback_${this._uid}`);
+    }
+    Formio.setBaseUrl(this.APP_URL)
+    Formio.registerPlugin(loopbackGetPlugin, `moveCallsToLoopback_${this._uid}`)
 
     this.initializeForm()
       .then(() => {
-        this.setupForm();
+        this.setupForm()
       })
-      .catch((err) => {
+      .catch(err => {
         /* eslint-disable no-console */
-        console.warn(err);
+        console.warn(err)
         /* eslint-enable no-console */
-      });
+      })
   }
 
   destroyed() {
     if (this.formio) {
-      Formio.deregisterPlugin(`moveCallsToLoopback_${this._uid}`);
-      this.formio.destroy(true);
+      Formio.deregisterPlugin(`moveCallsToLoopback_${this._uid}`)
+      this.formio.destroy(true)
     }
   }
 
@@ -182,75 +218,75 @@ export default class extends Vue {
             .render()
             .then(
               (formio: Formio): Formio => {
-                this.formio = formio;
-                return formio;
-              },
+                this.formio = formio
+                return formio
+              }
             )
             .catch((err: Error) => {
               /* eslint-disable no-console */
-              console.error(err);
+              console.error(err)
               /* eslint-enable no-console */
-            }),
-        );
+            })
+        )
       } else if (this.form) {
         resolve(
           new FormioForm(this.$refs.formio, this.form, this.options)
             .render()
             .then(
               (formio: Formio): Formio => {
-                this.formio = formio;
-                return formio;
-              },
+                this.formio = formio
+                return formio
+              }
             )
             .catch((err: Error) => {
               /* eslint-disable no-console */
-              console.error(err);
+              console.error(err)
               /* eslint-enable no-console */
-            }),
-        );
+            })
+        )
       } else {
         // If we get to here there is no src or form
-        reject('Must set src or form attribute');
+        reject('Must set src or form attribute')
       }
-    });
+    })
   }
 
   setupForm() {
     if (!this.formio) {
-      return;
+      return
     }
     if (this.submission) {
-      this.formio.submission = this.submission;
+      this.formio.submission = this.submission
     }
 
     if (this.url) {
-      this.formio.url = this.url;
+      this.formio.url = this.url
     }
 
     this.formio.language = this.language ? this.language : 'en'
 
     this.formio.events.onAny((...args: any[]) => {
-      const eventParts = args[0].split('.');
+      const eventParts = args[0].split('.')
 
       // Only handle formio events.
       if (eventParts[0] !== 'formio' || eventParts.length !== 2) {
-        return;
+        return
       }
 
       // Remove formio. from event.
-      args[0] = eventParts[1];
+      args[0] = eventParts[1]
 
-      this.$emit.apply(this, args);
+      this.$emit.apply(this, args)
 
       // Emit custom events under their own name as well.
       if (eventParts[1] === 'customEvent') {
-        args[0] = args[1].type;
-        this.$emit.apply(this, args);
+        args[0] = args[1].type
+        this.$emit.apply(this, args)
       }
-    });
+    })
   }
 
   render(createElement: any) {
-    return createElement('div', { ref: 'formio' });
+    return createElement('div', { ref: 'formio' })
   }
 }
